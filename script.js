93d229479f1f4588b76c93d6a7e38bba8d52ca71
faikarf3 <121655926+faikarf3@users.noreply.github.com>
2023-08-02 16:15:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
+}
+
+// Close dropdown when clicking outside
+window.onclick = function (event) {
+    if (!event.target.matches('.settings-icon')) {
+        var dropdowns = document.getElementsByClassName("dropdown");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
