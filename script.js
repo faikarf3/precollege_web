0553b073f7092f81b43633ece53f560de47ba2e0
faikarf3 <121655926+faikarf3@users.noreply.github.com>
2023-08-16 10:50:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.settings-icon')) {
+        var dropdowns = document.getElementsByClassName("dropdown");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
